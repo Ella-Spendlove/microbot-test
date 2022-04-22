b60fdbdba113eb1ea67951631f85fc89b2a29ced
Ella-Spendlove <102975888+Ella-Spendlove@users.noreply.github.com>
2022-04-22 08:43:44 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,24 @@
+input.onButtonPressed(Button.A, function () {
+	
+})
+input.onButtonPressed(Button.B, function () {
+    pins.analogWritePin(AnalogPin.P0, 0)
+    pins.analogWritePin(AnalogPin.P1, 0)
+})
 basic.forever(function () {
     pins.analogWritePin(AnalogPin.P0, 100)
     pins.analogWritePin(AnalogPin.P1, 100)
+    basic.pause(2000)
+    pins.analogWritePin(AnalogPin.P0, 10)
+    pins.analogWritePin(AnalogPin.P1, 10)
+    basic.pause(2000)
+    pins.analogWritePin(AnalogPin.P0, 10)
+    pins.analogWritePin(AnalogPin.P1, 100)
+    basic.pause(200)
+    pins.analogWritePin(AnalogPin.P0, 10)
+    pins.analogWritePin(AnalogPin.P1, 10)
+    basic.pause(500)
+    pins.analogWritePin(AnalogPin.P0, 10)
+    pins.analogWritePin(AnalogPin.P1, 100)
+    basic.pause(200)
 })
